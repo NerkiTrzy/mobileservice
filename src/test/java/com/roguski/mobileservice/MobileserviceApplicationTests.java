887a5292f53abcbceaa6c23143437d16e17d8212
Przemysław Roguski <przemyslaw.roguski@invicta.pl>
2018-11-01 18:00:15 +0100

    Initial spring boot application

---
+++ src/test/java/com/roguski/mobileservice/MobileserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.roguski.mobileservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobileserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
